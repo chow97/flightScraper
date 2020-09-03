69d9ec813c8fef65f9eddd40c9d2c6ff483ee0b2
Chow <chow@Chows-MacBook-Pro.local>
2020-09-04 07:38:40 +0800

    closed popup

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome()
 
 url = 'https://www.kayak.com/flights/KUL-SEA/2021-02-01?sort=bestflight_a&fs=airlines=~NH'
 
-driver.get(url)
+#url = 'https://the-internet.herokuapp.com/key_presses'
 
-driver
+driver.get(url)
+time.sleep(3)
+
+webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
